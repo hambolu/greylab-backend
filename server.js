b8ef1d2ba439eadb7d3e5e4bb31920c45dfe9301
Ouches Technology <henryhambolu@gmail.com>
2024-01-15 09:36:20 +0100

    updated

--- server.js
+++ server.js
@@ -18,40 +18,40 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Connect to MongoDB and handle errors properly
-// async function connectToMongo() {
-//   try {
-//     const client = await MongoClient.connect(process.env.DB_CONNECTION, {
-//       useUnifiedTopology: true,
-//       useNewUrlParser: true,
-//       ssl: true,
-//       tls: true,
-//     });
+async function connectToMongo() {
+  try {
+    const client = await MongoClient.connect(process.env.DB_CONNECTION, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      ssl: true,
+      tls: true,
+    });
 
-//     // Access the database using the client
-//     app.locals.db = client.db();
-//     console.log('Successfully connected to MongoDB.');
-//   } catch (err) {
-//     console.error('Connection error', err, process.env.DB_CONNECTION);
-//     process.exit(1);
-//   }
-// }
+    // Access the database using the client
+    app.locals.db = client.db();
+    console.log('Successfully connected to MongoDB.');
+  } catch (err) {
+    console.error('Connection error', err, process.env.DB_CONNECTION);
+    process.exit(1);
+  }
+}
 
 // // Initialize database and roles after connection
-// async function initialize() {
-//   const db = app.locals.db;
+async function initialize() {
+  const db = app.locals.db;
 
-//   const Role = db.collection('roles');
+  const Role = db.collection('roles');
 
-//   const count = await Role.countDocuments();
+  const count = await Role.countDocuments();
 
-//   if (!count) {
-//     await Role.insertMany([
-//       { name: 'developer' },
-//       { name: 'admin' },
-//       { name: 'user' },
-//     ]);
-//   }
-// }
+  if (!count) {
+    await Role.insertMany([
+      { name: 'developer' },
+      { name: 'admin' },
+      { name: 'user' },
+    ]);
+  }
+}
 
 // Root route handler
 app.get('/', (req, res) => {
@@ -81,8 +81,8 @@ app.delete('/delete-user/:id', [authJwt.authenticateToken], userControllers.dele
 
 // Start the server
 (async () => {
-  //await connectToMongo();
-  //await initialize();
+  await connectToMongo();
+  await initialize();
 
   const PORT = process.env.PORT || 8080;
   app.listen(PORT, () => {
