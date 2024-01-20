fe92364549b696a88393df9384b7c932d18c92ef
Ouches Technology <henryhambolu@gmail.com>
2024-01-20 15:59:50 +0100

    updated

--- models/user.model.js
+++ models/user.model.js
@@ -1,35 +1,33 @@
 const mongoose = require('mongoose');
 
-const UserSchema = mongoose.Schema({
-
-    login: {
-        type: String,
-    },
-    twitterProfile: {
-        type: Object,
-    },
-    discordProfile: {
-        type: Object,
-    },
-    venomAddress: {
-        type: String,
-        required: true,
-    },
-    roles: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Role"
-    },
-    created_at: {
-        type: Date,
-        default: Date.now
-    },
-    updated_at: {
-        type: Date,
-        default: Date.now
-    },
-    
-
+const userSchema = new mongoose.Schema({
+  login: {
+    type: String,
+  },
+  twitterProfile: {
+    type: Object,
+  },
+  discordProfile: {
+    type: Object,
+  },
+  venomAddress: {
+    type: String,
+    required: true,
+  },
+  roles: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Role',
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
+const User = mongoose.model('User', userSchema);
 
-module.exports = mongoose.model('Users', UserSchema);
+module.exports = User;
